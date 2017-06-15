576e72a36e2483b8eaef5675c137ef9e2984e314
Bryan <artificialknowledge217@gmail.com>
2017-06-15 19:02:10 -0400

    get user endpoint & import user & finance models

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require('../model/userModel');
+const { FinancialProfile } = require('../model/financesModel');
 
 //routes
+
+//user routes
+router.get('/users/:userId', (req, res) => {
+	//find the user by the id and then 
+	//send it. 
+	User 
+		.findById(req.params.userId, function (err, user) {
+			if (err) {
+				console.log(err);
+				res.json({ userNotFound: 'User does not exist in system' });
+			}
+			return user
+		})
+		.then(user => {
+			res.status(200).json(user.apiRepr());
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ errorMessage: 'Oops! Something went wrong!' });
+		})
+})
+
+//financial profile routes
 
 module.exports = { router };
