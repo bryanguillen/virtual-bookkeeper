cc21ff879657a54f4017904826e33e85fb75b0cc
Bryan <artificialknowledge217@gmail.com>
2017-06-15 19:47:06 -0400

    remove logic from route file

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('../model/userModel');
-const { FinancialProfile } = require('../model/financesModel');
+const { userController } = require('../controller/userController');
 
 //routes
 
 //user routes
-router.get('/users/:userId', (req, res) => {
-	//find the user by the id and then 
-	//send it. 
-	User 
-		.findById(req.params.userId, function (err, user) {
-			if (err) {
-				console.log(err);
-				res.json({ userNotFound: 'User does not exist in system' });
-			}
-			return user
-		})
-		.then(user => {
-			res.status(200).json(user.apiRepr());
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(500).json({ errorMessage: 'Oops! Something went wrong!' });
-		})
-})
+router.get('/users/:userId', userController.getUser);
 
 //financial profile routes
+
 
 module.exports = { router };
