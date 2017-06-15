cd0929d60c64f6cf85fd23eed64e751a436b87b1
Bryan <artificialknowledge217@gmail.com>
2017-06-15 18:44:47 -0400

    add run and close servers along with require.main

--- server.js
+++ server.js
@@ -11,6 +11,42 @@ app.use(express.static('public')); //subject to change static files directory
 app.use(morgan('common'));
 app.use('/', router);
 
-app.listen(3000, function () {
-	console.log('Your app is now listening on port 3000!');
-})
+function runServer () {
+	return new Promise((resolve, reject) => { 
+		mongoose.connect('mongodb://localhost/vb_app_test', err => {
+			if(err) {
+				console.log(err);
+				return reject(err);
+			}
+		})
+		server = app.listen(process.env.PORT || 3000, function() {
+			//this uri is going to change below to a parameter instead
+			console.log(`Your application is listening on port 3000 and db uri vb_app_test`); 
+			resolve();
+		})
+		.on('error', err => {
+			mongoose.disconnect();
+			return reject(err)
+		})
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+	return new Promise((resolve, reject) => {
+		console.log('Killing the server now!')
+		server.close(err => {
+			if(err) {
+				return reject(err);
+			}
+			resolve();
+		});
+	});
+	});
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = { app, runServer, closeServer };
