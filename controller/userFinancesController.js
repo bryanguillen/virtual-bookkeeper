33cc3766262af10ac15929301920fc4e0929ce9d
Bryan <artificialknowledge217@gmail.com>
2017-06-15 20:12:45 -0400

    add getProfile userFinancesController

--- controller/userFinancesController.js
+++ controller/userFinancesController.js
@@ -1,2 +1,25 @@
 const { FinancialProfile } = require('../model/financesModel');
 
+financialProfileController ={ 
+	getProfile: function (req, res) {
+		//first we need to find the user via the user id
+		//which is going to be provided from the params
+		FinancialProfile
+			.findOne({ 'user': req.params.userId }, function (err, profile) {
+				if (err) {
+					console.log(err);
+					return res.json({ notFound: 'The page you are looking for does not exist' });
+				}
+				return profile;
+			}) 
+			.then(profile => {
+				res.status(200).json(profile.apiRepr());
+			})
+			.catch(err => {
+				console.log(err);
+				res.status(500).json({ errorMessage: 'Internal Server Error' });
+			})
+	}
+}
+
+module.exports = { financialProfileController };
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { userController } = require('../controller/userController');
-
-//routes
+const { financialProfileController } = require('../controller/userFinancesController');
 
 //user routes
 router.get('/users/:userId', userController.getUser);
 
 //financial profile routes
-
+router.get('/users/:userId/finances', financialProfileController.getProfile);
 
 module.exports = { router };
