686e7c62e7d2a2ee7d6fdf9ac77f97bb5c66c0c1
Bryan <artificialknowledge217@gmail.com>
2017-06-15 22:57:55 -0400

    create post endpoint & handle promise rejection with catch try

--- controller/userFinancesController.js
+++ controller/userFinancesController.js
@@ -1,14 +1,12 @@
 const { FinancialProfile } = require('../model/financesModel');
+const { User } = require('../model/userModel');
 
 financialProfileController ={ 
 	getProfile: function (req, res) {
-		//first we need to find the user via the user id
-		//which is going to be provided from the params
 		FinancialProfile
 			.findOne({ 'user': req.params.userId }, function (err, profile) {
 				if (err) {
-					console.log(err);
-					return res.json({ notFound: 'The page you are looking for does not exist' });
+					return res.status(400).end('Bad Request');
 				}
 				return profile;
 			}) 
@@ -16,9 +14,41 @@ financialProfileController ={
 				res.status(200).json(profile.apiRepr());
 			})
 			.catch(err => {
-				console.log(err);
 				res.status(500).json({ errorMessage: 'Internal Server Error' });
 			})
+	},
+
+	createProfile: function (req, res) {
+		if (!req.body) {
+			return res.status(400).json({ errorMessage: 'Must include a request body!' })
+		}
+
+		let newProfile = new FinancialProfile ({
+			'user': req.body.user,
+			'totalSaved': req.body.totalSaved,
+			'monthlyIncome': req.body.monthlyIncome,
+			'expenditures': req.body.expenditures
+		})
+
+		User 
+			.findById(req.body.user, function (err, user) {
+				if (err) {
+					res.status(400).end('User Does Not Exist');					
+				}
+			})
+
+		newProfile.save(function (err, profile) {
+			if (err) {
+				res.status(500).end('Internal Server Error');
+			}
+
+			try { //handling the async flow for now. 
+				res.status(201).json(profile.apiRepr());
+			}
+			catch(err) {
+				res.status(500).end('Internal Server Error');
+			}
+		})
 	}
 }
 
