77a4940cb681cf8d01859405dc47d23de85a7cd6
Bryan <artificialknowledge217@gmail.com>
2017-06-19 15:17:34 -0400

    make type number for amount

--- model/expenditureModel.js
+++ model/expenditureModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const expenditureSchema = mongoose.Schema({
 	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-	amount: {type: String, required: true},
+	amount: {type: Number, required: true}, //changed the amount type to number it was string 
 	expenseName: {type: String, required: true},
 	dateCreated: {type: Date, default: Date.now()}
 })
@@ -10,7 +10,7 @@ const expenditureSchema = mongoose.Schema({
 
 expenditureSchema.methods.expenditureAPIRepr = function() {
 	return {
-		_id: this._id, //newly added
+		_id: this._id,
 		user: this.user,
 		amount: this.amount,
 		expenseName: this.expenseName
