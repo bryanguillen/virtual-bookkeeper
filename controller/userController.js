04d7ff43fc777ba4c4079fc485711549d46a1fe0
Bryan <artificialknowledge217@gmail.com>
2017-06-19 02:32:00 -0400

    programmitcally enter dates for query

--- controller/userController.js
+++ controller/userController.js
@@ -1,20 +1,61 @@
 const { User } = require('../model/userModel');
+const { Expenditure } = require('../model/expenditureModel');
+const { userHelper } = require('./helpers/helper'); //keep an eye on the memory leak warning
 
 const userController = {
 	getProfile: function (req, res) { 
-		User 
-			.findById(req.params.userId, function (err, profile) { //beware using params
+		//temporary way of doing this. 
+		let currentTime = new Date(Date.now()),
+			year = currentTime.getFullYear(),
+			month = currentTime.getMonth() + 1;
+
+		let lastDay = userHelper.dateLookup(month.toString());
+
+		if (month < 10) {
+			month = '0' + month;
+		}
+
+		let beginMonth = year + '/' + month + '/01',
+			endMonth = year + '/' + month + '/' + lastDay;
+
+		let query = { 
+						user: req.params.userId, 
+						dateCreated: { 
+							$gte: new Date(beginMonth), 
+							$lt: new Date(endMonth) 
+						} 
+					};
+		Expenditure
+			.find(query, function (err, docs) {
 				if (err) {
-					res.status(500).json({ errorMessage: 'Internal Server Error' });
-				}
-				return profile
+					res.status(500).json({ errorMessage: 'Inernal Server Error' });
+				} 
+				return docs;
 			})
-			.then(profile => {
-				res.status(200).json(profile.apiRepr());
+			.then(docs => {
+				let formattedDocs = [];
+				for (let i=0, length=docs.length; i<docs.length; i++) {
+					let currentDoc = docs[i];
+					formattedDocs.push(currentDoc.expenditureAPIRepr());
+				}
+				return formattedDocs;
+			})
+			.then(formattedDocs => {
+				User
+					.findById(req.params.userId)
+					.populate('expenditures')
+					.exec(function (err, profile) {
+						if (err) {
+							res.status(500).json({ errorMessage: 'Inernal Server Error' });
+						}
+						res.status(200).json(profile.profileAPIRepr(formattedDocs));
+					})
 			})
 			.catch(err => {
+				console.log(err);
 				res.status(500).json({ errorMessage: 'Internal Server Error' });
 			})
+
 	},
 
 	updateProfile: function (req, res) {
