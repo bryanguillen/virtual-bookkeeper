9ff71247637164b04e38cdad368e03b32caf9479
Bryan <artificialknowledge217@gmail.com>
2017-06-20 10:15:29 -0400

    add handlers for input fields

--- client/src/components/ExpenseInput.js
+++ client/src/components/ExpenseInput.js
@@ -6,20 +6,27 @@ export default class ExpenseInput extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			expenseName: ''
+			expenseName: '',
+			amount: 0
 		}
 	
-		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleExpenseName = this.handleExpenseName.bind(this);
+		this.handleAmount = this.handleAmount.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	handleInputChange (e) {
-		this.setState({ expenseName: event.target.value });
+	handleExpenseName (e) {
+		this.setState({ expenseName: e.target.value });
+	}
+
+	handleAmount (e) {
+		this.setState({ amount: e.target.value });
 	}
 
 	handleSubmit (e) {
 		e.preventDefault();
 		console.log('The Input Submitted: ' + this.state.expenseName);
+		console.log('The number Submitted: ' + this.state.amount);
 	}
 
 	render(){
@@ -28,14 +35,15 @@ export default class ExpenseInput extends React.Component {
 				<form onSubmit={this.handleSubmit}>
 					<label>
 						Expense Name: 
-						<input type="text" value={this.state.expenseName} onChange={this.handleInputChange} />
+						<input type="text" value={this.state.expenseName} onChange={this.handleExpenseName} />
 					</label>
 					<label>
 						Expense Amount: 
-						<input type="number" value={this.state.value} onChange={this.handleInputChange} />
+						<input type="number" value={this.state.amount} onChange={this.handleAmount} />
 					</label>
+					<button type="submit">Create Expense</button>
 				</form>
 			</div>
 		);
 	}
-expenseName
+}
