6950e72400a5318537c5d777d4dfed4e3fa141b5
Bryan <artificialknowledge217@gmail.com>
2017-06-20 22:43:06 -0400

    add conditional rendering to component with new state attr error

--- client/src/components/ExpenseInput.js
+++ client/src/components/ExpenseInput.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles/ExpenseInput.css';
-//WORK IN PROGRESS!!!!!!!!!!!!!!!!!!
 
 export default class ExpenseInput extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
 			expenseName: '',
-			amount: 0
+			amount: 0,
+			error: false
 		}
 	
 		this.handleExpenseName = this.handleExpenseName.bind(this);
@@ -25,24 +25,45 @@ export default class ExpenseInput extends React.Component {
 
 	handleSubmit (e) {
 		e.preventDefault();
-		console.log('The Input Submitted: ' + this.state.expenseName);
-		console.log('The number Submitted: ' + this.state.amount);
+		let userInput = this.state.expenseName;
+		if (userInput.trim().length === 0) {
+			return this.setState(prevState => ({
+      					error: !prevState.error
+    				}));
+    	}
+    	//here we take the input and send it to the server.
+    	this.setState({ 
+    		expenseName: '',
+    		amount: 0,
+    		error: false
+    	})
 	}
 
 	render(){
+		let form =  <form onSubmit={this.handleSubmit}>
+					    <label>
+						    Expense Name: 
+						    <input name="expense-name" type="text" value={this.state.expenseName} onChange={this.handleExpenseName} />
+					    </label>
+					    <label>
+						    Expense Amount: 
+						    <input name="amount" type="number" value={this.state.amount} onChange={this.handleAmount} />
+					    </label>
+					    <button type="submit">Create Expense</button>
+				    </form>;
+		let errorMessage = <span>You must fill out both fields!</span>;
+		let error = this.state.error;
+		if (error) {
+			return (
+				<div className="expense-input-wrapper">
+					{errorMessage}
+					{form}					
+				</div>
+			);
+		}
 		return (
 			<div className="expense-input-wrapper">
-				<form onSubmit={this.handleSubmit}>
-					<label>
-						Expense Name: 
-						<input type="text" value={this.state.expenseName} onChange={this.handleExpenseName} />
-					</label>
-					<label>
-						Expense Amount: 
-						<input type="number" value={this.state.amount} onChange={this.handleAmount} />
-					</label>
-					<button type="submit">Create Expense</button>
-				</form>
+				{form}					
 			</div>
 		);
 	}
