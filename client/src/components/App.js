1856805a6ee998462ec00858d34fc2f6cd88d457
Bryan <artificialknowledge217@gmail.com>
2017-06-20 22:44:09 -0400

    rearrange import statements

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import FinancialStat from './FinancialStat';
 import Greetings from './Greetings';
+import FinancialStat from './FinancialStat';
+import ExpenseInput from './ExpenseInput';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <FinancialStat description={'Expenses This Month: '} value={500}/>
         <FinancialStat description={'Net Income This Month: '} value={1500}/>
         <FinancialStat description={'Goal For The Month: '} value={750}/>
+        <ExpenseInput />
       </div>
     );
   }
