0ef5cee0a80261825b77bc2be8ccc5aa7b41289f
Bryan <artificialknowledge217@gmail.com>
2017-06-21 22:51:21 -0400

    add netIncome

--- model/userModel.js
+++ model/userModel.js
@@ -10,13 +10,20 @@ const userProfileSchema = mongoose.Schema({ //schema or better yet, known as the
 });
 
 userProfileSchema.methods.profileAPIRepr = function(formattedDocs, totalExp) { //toalExp is the total for the month
+	//all of the keys below contain relevant values for 
+	//a user to see how they are doing on savings this month 
+	//calculate relevant info for profile apiRepr
+	let netIncome = this.monthlyIncome - totalExp; 
+	
 	return {
 		username: this.username,
 		totalSaved: this.totalSaved,
 		monthlyIncome: this.monthlyIncome,
 		monthlySpend: totalExp,
+		netIncome, //newly added
 		expenditures: formattedDocs
 	}
+
 }
 
 const User = mongoose.model('User', userProfileSchema); //creates the User collection
