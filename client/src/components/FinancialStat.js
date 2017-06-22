8f702879a9ad51cf1a576cbfd106b9922914d67d
Bryan <artificialknowledge217@gmail.com>
2017-06-22 01:57:39 -0400

    add condition rendering

--- client/src/components/FinancialStat.js
+++ client/src/components/FinancialStat.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import './styles/FinancialStat.css';
+import EditStatField from './EditStatField';
 
 export default function FinancialStat (props) {
-	return (
+        if (props.editing) {
+            return (
+                <div className="col-md-3">
+                    <div className="stats-container">
+                        <span className="stat-descr">
+                            {props.description}
+                        </span>
+                    </div>
+                    <div className="stats-container">
+                        <EditStatField 
+                        onChange={props.onChange}
+                        value={props.value}
+                        name={props.name}
+                        />
+                    </div>
+                </div>
+            );        
+        }
+           
+        return(
             <div className="col-md-3">
                 <div className="stats-container">
                     <span className="stat-descr">
-        		        {props.description}
-        	       </span>
+                        {props.description}
+                   </span>
                 </div>
                 <div className="stats-container">
-        	        <span className="stat-value">
-        		        {props.value}
-        	        </span>
+                    <span className="stat-value">
+                        ${props.value}
+                    </span>
                 </div>
             </div>
-	);
+        )
 }
