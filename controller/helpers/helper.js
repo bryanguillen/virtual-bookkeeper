b615a0a767cf26eaf53bf89a8576b0501adebe89
Bryan <artificialknowledge217@gmail.com>
2017-06-24 01:15:18 -0400

    filler commit w/ broken code incase hardrive corruption

--- controller/helpers/helper.js
+++ controller/helpers/helper.js
@@ -1,9 +1,29 @@
 const userHelper = {
-	getQueryDates: function () {
+	monthLookup: function (month) {
+		let monthNum = month.toString();
+
+		months = {
+			'1': 'Jan',
+			'2': 'Feb',
+			'3': 'Mar',
+			'4': 'Apr',
+			'5': 'May',
+			'6': 'Jun',
+			'7': 'Jul',
+			'8': 'Aug',
+			'9': 'Sep',
+			'10': 'Oct',
+			'11': 'Nov',
+			'12': 'Dec'
+		}
+		return months[monthNum];
+	},
+
+	getMonthlyQuery: function () {
 		
-		let currentTime = new Date(Date.now()),
-			year = currentTime.getFullYear(),
-			month = currentTime.getMonth() + 1,
+		let currentDate = new Date(Date.now()),
+			year = currentDate.getFullYear(),
+			month = currentDate.getMonth() + 1,
 			nextMonth;
 
 		if (month === 12) {
@@ -27,7 +47,98 @@ const userHelper = {
 		} 
 
 		return queryDates;
-	}
+	},
+
+	activeTimeframe: function (signupDate) {
+		//return timeframe for the user's active months
+		//in other words, if the user signed up in May of 2016
+		//this function will return 
+		//{ start: {year:2016, month: 5}, end: {year:2017, month: 7} }
+
+		let currentDate = new Date(Date.now()),
+			dates = [signupDate, currentDate],
+			keys = ['start', 'end'],
+			timeframe = {};
+
+		for (let i=0; i<dates.length; i++) {
+			let typeOfDate = dates[i],
+				currentKeys = keys[i],
+				monthAndYear = {};
+
+			//getting specific numbers for signupDate and currentDate...
+			let year = typeOfDate.getFullYear(),
+			month = typeOfDate.getMonth() + 1;
+
+			monthAndYear = {
+				year, 
+				month
+			};
+			
+			timeframe[currentKeys] = monthAndYear;
+		}
+		
+		return timeframe;
+	},
+
+	monthsActive: function (timeframe) {
+		//reutrn all of the months active in array 
+		//of obj containting all of the months between 
+		//the user's signup date and the current month
+
+		let activeMonths = [];
+		let endYear = timeframe.end.year,
+			endMonth = timeframe.end.month;
+		//might not work
+		let startMonth = timeframe.start.month,
+			startYear = timeframe.start.year;
+		
+		let i=startYear, iTwo=startMonth;
+		while (i <= endYear ) {
+			while (iTwo <= endMonth) {
+				let monthAndYear = {
+					year: iTwo,
+					month: this.monthLookup(i)
+				}
+				activeMonths.unshift(monthAndYear);
+
+				if (iTwo === 12) {
+					iTwo = 1;
+					i += 1;
+				}				
+				iTwo += 1;
+
+			}
+		}
+
+		//get the amount of times you want to increment the counter
+
+
+		// while(month !== endMonth+1 && year !== endYr) {
+		// 	//Right here we need to look up the month and 
+		// 	//then get the month acronym. Next, we get the year.
+		// 	//After that, we add those two to an array .  
+
+		// 	month = this.monthLookup(month);
+
+
+		// 	monthAndYear = {
+		// 		year, 
+		// 		month
+		// 	}
+
+		// 	activeMonths.unshift(monthAndYear);
+
+		// 	if (month === 12) {
+		// 		month = 1;
+		// 		year += 1;
+		// 	}
+		// 	else {
+		// 		month  += 1;
+		// 	}
+		// }
+
+		// return {endYr, endMonth, month, year};
+	}	
 }
 
 module.exports = { userHelper };
--- controller/userProfileController.js
+++ controller/userProfileController.js
@@ -2,9 +2,11 @@ const { User } = require('../model/userModel');
 const { Expenditure } = require('../model/expenditureModel');
 const { userHelper } = require('./helpers/helper'); //keep an eye on the memory leak warning
 
+//bryang01 594dd4d447f990bb6450622a id
+
 const userProfileController = {
 	getProfile: function (req, res) {  
-		let queryDates = userHelper.getQueryDates();
+		let queryDates = userHelper.getMonthlyQuery();
 
 		let query = { 
 						user: req.params.userId, 
@@ -65,6 +67,41 @@ const userProfileController = {
 				}
 
 				res.status(204).end();
+			})
+	},
+
+	getProfileHistory: function (req, res) {
+		//here we want to get all of the post 
+		//that the user has made throughout the past months
+		//some things to keep in mind, we need an index for partial loading, 
+		//and a little more. 
+
+		//so we need to know a begin point and end point. that being when the user signed up. 
+		// for the begin and current for everything in between then. 
+		//getHistoryQuery
+
+		User
+			.findById(req.params.userId, function (err, user) {
+				if (err) {
+					res.status(500).json({ errorMessage: 'Internal Server Error' });
+				}
+
+				return user;
+			})
+			.then(user => {
+				//see activeTimeframe comments for docs
+				let timeframe = userHelper.activeTimeframe(user.created_At); 
+				console.log(timeframe);
+				//right here we want to return something like 
+				//userHelper.monthsActive(timeframe)
+				//which returns an array of objects containing months and years
+				//something like May | 2017 
+				let months = userHelper.monthsActive(timeframe);
+				console.log(months);
+				res.status(200).send('cool!');
+			})
+			.catch(err => {
+				res.status(500).json({ errorMessage: 'Internal Server Error' });
 			})
 	}
 } 
