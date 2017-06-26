55b7ba2c03aab69a41b90e75df21adaf85d0c4b0
Bryan <artificialknowledge217@gmail.com>
2017-06-26 11:34:13 -0400

    add getMonthlyRecord

--- controller/monthlyController.js
+++ controller/monthlyController.js
@@ -3,11 +3,31 @@ const { User } = require('../model/userModel');
 const { controllerHelper } = require('./helpers/helper');
 
 const monthlyController = {
+	getMonthlyRecord: function (req, res) {
+		let reqParams = req.params,
+			user = reqParams.userId,
+			month = reqParams.month,
+			year = reqParams.year;
+
+		//get a single record
+		Month
+			.findOne({ user, month, year }, function (err, record) {
+				if (err) {
+					return res.status(500).json({ errorMessage: 'Internal Server Error' })
+				}
+				return record;
+			})
+			.then(record => {
+				res.status(200).json(record.monthlyAPIRepr());
+			})
+
+	},
+
 	createMonthlyRecord: function (req, res) {
 		//create the current month in question for the user in question
 		let currentDate = controllerHelper.getCurrentMonth(),
 			year = currentDate.year,
-			month = currentDate.month,
+			month = currentDate.month.toLowerCase(), 
 			user = req.params.userId;
 
 		let newMonth = new Month ({
@@ -20,6 +40,8 @@ const monthlyController = {
 		if ( typeof year !== 'number' || typeof month !== 'string' ) {
 			return res.status(400).json({ errorMessage: 'Bad request!' });
 		}
+
+		//add for a user params that does not exists when possible
 
 		//first protect against another month submission for the same user and then save that month and return it
 		Month
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { userProfileController } = require('../controller/userProfileController');
 const { authenticationController } = require('../controller/authenticationController');
+const { userProfileController } = require('../controller/userProfileController');
 const { monthlyController } = require('../controller/monthlyController');
 
-//user profile routes
+//authentication
 router.post('/users/new', authenticationController.createNewUser); 
+
+//user authenticated endpoints
 router.get('/users/:userId', userProfileController.getProfile);
 router.put('/users/:userId/finances', userProfileController.updateProfile);
-router.get('/users/:userId/history', userProfileController.getProfileHistory);
 
+//monthly endpoints
+router.get('/users/:userId/:month/:year', monthlyController.getMonthlyRecord);
 router.post('/users/:userId/new-month', monthlyController.createMonthlyRecord);
 
 module.exports = { router };
