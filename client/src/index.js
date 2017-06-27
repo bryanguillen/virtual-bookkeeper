8859c3cc35965b24bdd7ddafafb5dc679a3c9815
Bryan <artificialknowledge217@gmail.com>
2017-06-26 21:59:02 -0400

    change the title of the application

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './components/App';
+import UserHome from './components/UserHome';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UserHome />, document.getElementById('root'));
 registerServiceWorker();
